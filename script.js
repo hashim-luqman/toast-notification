3abe71e56ec19bf45d5a0549a0b424fabbb9b022
hashim-luqman <luqmanhashim474@gmail.com>
2020-09-28 15:21:40 +0300

    toast-notification

---
+++ script.js
@@ -0,0 +1,14 @@
+const btn = document.getElementById("btn");
+
+btn.addEventListener('click',() =>{
+    createNotification();
+});
+function createNotification(){
+    const notif = document.createElement('div');
+    notif.classList.add('toast');
+    notif.innerText = "this is  awesome";
+    container.appendChild(notif);
+    setTimeout(() =>{
+        notif.remove();
+    },3000);
+}
